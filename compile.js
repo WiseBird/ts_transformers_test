9d6124c2a2dedfa8acd066faad85b538998580fa
sergey.sokolov <sergey.sokolov@behavox.com>
2017-08-09 18:29:42 +0300

    Added example with compiler api

---
+++ compile.js
@@ -0,0 +1,12 @@
+const ts = require('typescript');
+const uppercaseTransformer = require('./uppercase-transformer');
+const fileName = `${__dirname}/src/module.ts`;
+
+const program = ts.createProgram([fileName], {module: 'commonjs', target: ts.ScriptTarget.ES5});
+
+const transformers = {before: [uppercaseTransformer], after: []};
+const {emitSkipped, diagnostics} = program.emit(undefined, undefined, undefined, false, transformers);
+
+if (emitSkipped) {
+    throw new Error(diagnostics.map(diagnostic => diagnostic.messageText).join('\n'));
+}
